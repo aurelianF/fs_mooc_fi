3340b329095302655c0d2e8b593fa9c326bd50d1
basileu <basil.boli@yahoo.com>
2023-06-26 06:52:21 +0300

    Refactored variables from App into an array of objects, still pass them individually
    Signed-off-by: basileu <basil.boli@yahoo.com>

--- part1/part1_exercise/src/App.js
+++ part1/part1_exercise/src/App.js
@@ -18,9 +18,6 @@ const Content = (props) => {
       <Part part={props.part1} exercises={props.exercises1} />
       <Part part={props.part2} exercises={props.exercises2} />
       <Part part={props.part3} exercises={props.exercises3} />
-      {/* <p> {props.part1} {props.exercises1}</p>
-      <p> {props.part2} {props.exercises2}</p>
-      <p> {props.part3} {props.exercises3}</p> */}
     </>
   );
 };
@@ -36,27 +33,30 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
   return (
     <>
-      <Header course={course}  />
-      <Content part1={part1.name} exercises1={part1.exercises} part2={part2.name} 
-      exercises2={part2.exercises} part3={part3.name} exercises3={part3.exercises} />
-      <Total sum={part1.exercises  + part2.exercises + part3.exercises}/>
+      <Header course={course} />
+      <Content part1={parts[0].name} exercises1={parts[0].exercises} part2={parts[1].name}
+        exercises2={parts[1].exercises} part3={parts[2].name} exercises3={parts[2].exercises} />
+      <Total sum={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
 
     </>
-  )  
+  )
 }
 
 export default App
