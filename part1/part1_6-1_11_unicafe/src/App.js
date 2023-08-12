9bd47ba55fb72ee2804b8dded269cb98413eea64
basileu <basil.boli@yahoo.com>
2023-08-12 11:08:52 +0300

    Exercise 1.8
    Signed-off-by: basileu <basil.boli@yahoo.com>

--- part1/part1_6-1_11_unicafe/src/App.js
+++ part1/part1_6-1_11_unicafe/src/App.js
@@ -2,44 +2,54 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
 
-const Button = function (props) {
-  return (
-      <button onClick={props.handleClick}>
-          {props.text}
-      </button>
-  )
-}
-function App() {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0) 
-  const [bad, setBad] = useState(0) 
-  const [neutral, setNeutral] = useState(0)
+// a proper place to define a component
+const Statistics = ({good, bad, neutral, all, avgFB, totalFB, positiveFB}) => {
 
-  function giveFeedback(feedbackType) {
-    switch (feedbackType) {
-      case "good": setGood(good+1); break;
-      case "bad": setBad(bad+1); break;
-      case "neutral": setNeutral(neutral+1); break;
-      default: break;
-    }
-    console.log(good+"\n"+bad+"\n"+neutral);
-  }
-  let avgFB = (good+bad+neutral)/3;
-  let totalFB = good+bad+neutral;
-  let positiveFB = good/totalFB;
   return (
     <div>
-      <h1>give feedback</h1>
-        <Button handleClick={() => giveFeedback("good")} text="good"/>
-        <Button handleClick={() => giveFeedback("neutral")} text="neutral"/>
-        <Button handleClick={() => giveFeedback("bad")} text="bad"/>
-      <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
       <p>all {totalFB}</p>
       <p>average {avgFB}</p>
       <p>positive {positiveFB}</p>
+    </div>
+  )
+}
+
+const Button = function (props) {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+function App() {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+
+  function giveFeedback(feedbackType) {
+    switch (feedbackType) {
+      case "good": setGood(good + 1); break;
+      case "bad": setBad(bad + 1); break;
+      case "neutral": setNeutral(neutral + 1); break;
+      default: break;
+    }
+    console.log(good + "\n" + bad + "\n" + neutral);
+  }
+  let avgFB = (good + bad + neutral) / 3;
+  let totalFB = good + bad + neutral;
+  let positiveFB = good / totalFB;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => giveFeedback("good")} text="good" />
+      <Button handleClick={() => giveFeedback("neutral")} text="neutral" />
+      <Button handleClick={() => giveFeedback("bad")} text="bad" />
+      <h1>statistics</h1>
+      <Statistics good={good} bad={bad} neutral={neutral} avgFB={avgFB}totalFB={totalFB} positiveFB={positiveFB} />
     </div>
     // <div className="App">
     //   <header className="App-header">
