e986fbee65be0fcaf29301190fae5ee7a7156f2e
basileu <basil.boli@yahoo.com>
2023-08-21 08:54:10 +0300

    ex 2.8 Adding Phonebook numbers
    Signed-off-by: basileu <basil.boli@yahoo.com>

--- part2/part2_ex_phonebook/src/App.js
+++ part2/part2_ex_phonebook/src/App.js
@@ -12,11 +12,14 @@ function equals(a, b) {
 // main App component
 const App = () => {
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
+    console.log("addName called with event: ", event);
     event.preventDefault();
     const newPerson = {
-      name:newName,
+      name: newName,
+      number: newNumber
     }
 
     // marker for duplication
@@ -27,36 +30,42 @@ const App = () => {
         exists = true;
         alert(`${newPerson.name} is already added in the phonebook`);
         break;
-      } 
+      }
     }
     if (!exists) {
       setPersons(persons.concat(newPerson));
-      setNewName("");      
-    } 
-    console.log("Array at end of call ", persons);
+      setNewName("");
+      setNewNumber("");
+    }
   }
   const handleFieldChange = (event) => {
     setNewName(event.target.value);
+  }  
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   }
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    // { name: 'Arto Hellas' }
   ])
 
   return (
     <div>
       <div>debug: {newName}</div>
+      <div>debug: {newNumber}</div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleFieldChange} />
-
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-        {persons.map(pers => <p key={pers.name}>{pers.name}</p>)}
+      {persons.map(pers => <p key={pers.name}>{pers.name}</p>)}
     </div>
   )
 }
