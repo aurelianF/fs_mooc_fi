0eec52f7797d49ea7023f38d2f3336e63eaa011d
basileu <basil.boli@yahoo.com>
2023-08-22 06:46:37 +0300

    ex 2.9 Adding filter active onChange call
    Signed-off-by: basileu <basil.boli@yahoo.com>

--- part2/part2_ex_phonebook/src/App.js
+++ part2/part2_ex_phonebook/src/App.js
@@ -8,14 +8,22 @@ function equals(a, b) {
     return false;
   }
 }
-
 // main App component
 const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  const [filterVar, setfilterVar] = useState('')
+  // let filterVar= null;
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+  const [filteredList, setFilteredList] = useState([]);
+  
   const addName = (event) => {
-    console.log("addName called with event: ", event);
+    // console.log("addName called with event: ", event);
     event.preventDefault();
     const newPerson = {
       name: newName,
@@ -24,6 +32,8 @@ const App = () => {
 
     // marker for duplication
     let exists = false;
+
+    
     // iterate through entire array
     for (let index = 0; index < persons.length; index++) {
       if (equals(persons[index], newPerson)) {
@@ -36,23 +46,39 @@ const App = () => {
       setPersons(persons.concat(newPerson));
       setNewName("");
       setNewNumber("");
+      // setfilterVar("");
     }
+    
   }
   const handleFieldChange = (event) => {
     setNewName(event.target.value);
-  }  
+  }
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+  }  
+  const handleFilterInputChange = (event) => {
+    let filterVar1 = event.target.value;
+    setfilterVar(event.target.value);
+    // console.log("Filter: ", filterVar1);
+    setFilteredList(persons.filter(p=>{
+      // console.log("p.name: ", p.name);
+      // console.log(p.name.startsWith(filterVar1));
+      return (p.name.toLocaleLowerCase().startsWith(filterVar1.toLocaleLowerCase()))
+    }));
   }
-  const [persons, setPersons] = useState([
-    // { name: 'Arto Hellas' }
-  ])
+
 
   return (
     <div>
-      <div>debug: {newName}</div>
-      <div>debug: {newNumber}</div>
+      {/* <div>debug: {newName}</div> */}
+      {/* <div>debug: {newNumber}</div> */}
+      {/* <div>debug: {filterVar}</div> */}
       <h2>Phonebook</h2>
+      {/* <form> */}
+        <div>
+        filter shown with:<input type="text" onChange={handleFilterInputChange}></input>
+        </div>
+      {/* </form> */}
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleFieldChange} />
@@ -65,7 +91,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(pers => <p key={pers.name}>{pers.name}</p>)}
+      {/* {persons.map(pers => <p key={pers.name}>{pers.name}</p>)} */}
+      <p>filterVar {filterVar}</p>
+      {(filterVar!=="") ? filteredList.map(pers => <p key={pers.name}>{pers.name} {pers.number}</p>) :
+      persons.map(pers => <p key={pers.name}>{pers.name} {pers.number}</p>)}
+      {/* {filteredList.map(pers => <p key={pers.name}>{pers.name} {pers.number}</p>)} */}
     </div>
   )
 }
